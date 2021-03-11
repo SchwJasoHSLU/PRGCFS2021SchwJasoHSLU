300aae97d1bdd49817cf8e7a574a7895e5b42044
SchwJasoHSLU <schwe1997@gmail.com>
2021-03-11 20:24:02 +0100

    cleanup

---
+++ 21_workshop/Loops.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+	int main() 
+{
+	int sum	 = 0;
+	int start	 = 1;
+	int end   	 = 10;
+	
+	for( int i = start; i <= end; i ++ )
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+	
---
+++ 40_Excercises/04_CLA_Operators/Logic_OperatorQ3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+	int main(void) 
+{
+	int a = 0, b = 0;
+	printf("a: %d\n", ++a);
+	printf("b: %d\n", b++);
+	
+	printf("b: %d\n", b);
+	
+	return 0;
+}
+	
