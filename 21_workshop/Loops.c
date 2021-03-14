cc1e59b80363bbcdebbe0dcdfcb88e4d5573bd20
SchwJasoHSLU <schwe1997@gmail.com>
2021-03-14 12:15:19 +0100

    cleanup

--- 21_workshop/Loops.c
+++ 21_workshop/Loops.c
@@ -6,13 +6,12 @@
 	int start	 = 1;
 	int end   	 = 10;
 	
-	for( int i = start; i <= end; i ++ )
+	for( int i = start; i <= end; i )
+	{
+		sum = sum + i;
+	}
 	
-	
-	
-	
-	
-	
+	printf("the sum from %d to %d is equal to %d\n", 
+		start, end, sum);
 	return 0;
 }
-	
