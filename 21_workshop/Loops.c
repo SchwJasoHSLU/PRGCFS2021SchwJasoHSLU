673e125f7700db3cbac052377e08f4202ac04ed3
SchwJasoHSLU <schwe1997@gmail.com>
2021-03-25 20:27:35 +0100

    update

--- 21_workshop/Loops.c
+++ 21_workshop/Loops.c
@@ -4,14 +4,21 @@
 {
 	int sum	 = 0;
 	int start	 = 1;
-	int end   	 = 10;
+	int end   	 = 10000;
+	int max 	 = 555;
 	
-	for( int i = start; i <= end; i )
+	for( int i = start; i < end; i++)
 	{
 		sum = sum + i;
+		
+		if (sum > max) 
+		{
+			printf("with max. sum %d\n, the sum from %d to %d is equal to %d", 
+		max, start, i, sum );
+			break;
+		}	
+		
 	}
 	
-	printf("the sum from %d to %d is equal to %d\n", 
-		start, end, sum);
 	return 0;
 }
